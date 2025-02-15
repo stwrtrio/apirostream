a2acbbeca3f3d9ad79725fe05d989bef9acbdc55
stwrtrio <stwrtrio@gmail.com>
2025-02-16 03:42:13 +0700

    ubah nama file

--- build.bat
+++ build.bat
@@ -3,16 +3,16 @@
 :: Build untuk Windows
 set GOOS=windows
 set GOARCH=amd64
-go build -o bin\windows\loopshpere.exe main.go
+go build -o bin\windows\apirostream.exe main.go
 
 :: Build untuk macOS
 set GOOS=darwin
 set GOARCH=amd64
-go build -o bin\macos\loopshpere main.go
+go build -o bin\macos\apirostream main.go
 
 :: Build untuk Linux
 set GOOS=linux
 set GOARCH=amd64
-go build -o bin\linux\loopshpere main.go
+go build -o bin\linux\apirostream main.go
 
 echo Build selesai! Executable tersedia di folder bin.
--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Build untuk Windows
-GOOS=windows GOARCH=amd64 go build -o bin/windows/loopshpere.exe main.go
+GOOS=windows GOARCH=amd64 go build -o bin/windows/apirostream.exe main.go
 
 # Build untuk macOS
-GOOS=darwin GOARCH=amd64 go build -o bin/macos/loopshpere main.go
+GOOS=darwin GOARCH=amd64 go build -o bin/macos/apirostream main.go
 
 # Build untuk Linux
-GOOS=linux GOARCH=amd64 go build -o bin/linux/loopshpere main.go
+GOOS=linux GOARCH=amd64 go build -o bin/linux/apirostream main.go
 
 echo "Build selesai! Executable tersedia di folder bin."
